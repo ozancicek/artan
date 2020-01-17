14ef3cc11a5270369092cd006c212d8308e46444
ozan <ozancancicekci@gmail.com>
2020-01-17 22:09:53 +0900

    linear kalman filter doc

--- src/main/scala/com/ozancicek/artan/ml/filter/LinearKalmanFilter.scala
+++ src/main/scala/com/ozancicek/artan/ml/filter/LinearKalmanFilter.scala
@@ -26,6 +26,39 @@ import org.apache.spark.ml.{BLAS}
 import org.apache.spark.sql._
 
 
+/**
+ * Linear kalman filter, implemented with a stateful spark Transformer for running parallel filters /w spark
+ * dataframes. Transforms an input dataframe of noisy measurements to dataframe of state estimates using stateful
+ * spark transormations, which can be used in both streaming and batch applications.
+ *
+ * Assuming a state (x_k) with size n_s, and measurements (z_k) with size n_m, following matrices must be specified;
+ *
+ * - F_k, process model, matrix with dimensions (n_s, n_s)
+ * - H_k, measurement model, matrix with dimensions (n_s, n_m)
+ * - Q_k, process noise covariance, matrix with dimensions (n_s, n_s)
+ * - R_k, measurement noise covariance, matrix with dimensions (n_m, n_m)
+ * - B_k, optional control model, matrix with dimensions (n_s, n_control)
+ * - u_k, optional control vector, vector with size (n_control)
+ *
+ * The kalman filter will predict & estimate the state according to following equations
+ *
+ * State prediction:
+ *  x_k = F_k * x_k-1 + B_k * u_k + w_k
+ *
+ * Measurement incorporation:
+ *  z_k = H_k * x_k + v_k
+ *
+ * Where v_k and w_k are noise vectors drawn from zero mean, Q_k and R_k covariance distributions.
+ *
+ * The default values of system matrices will not give you a functioning filter, but they will be initialized
+ * with reasonable values given the state and measurement sizes. All of the inputs to the filter can
+ * be specified with a dataframe column which will allow you to have different value across measurements/filters,
+ * or you can specify a constant value across all measurements/filters.
+ *
+ *
+ * @param stateSize size of the state vector
+ * @param measurementSize size of the measurement vector
+ */
 class LinearKalmanFilter(
     val stateSize: Int,
     val measurementSize: Int,
